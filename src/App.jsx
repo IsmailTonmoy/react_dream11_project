1d1e549340eb821bdb8532c06311bc039303863a
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-10-26 19:55:02 +0600

    created project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='text-6xl bg-red-700'>Dream11</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
