9735db90e749c025712858824f9ebe9699b65e8b
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-10-27 11:32:01 +0600

    added data and picture

---
+++ data/data.js
@@ -0,0 +1,67 @@
+const data=[
+    {
+      "id": 1,
+      "name": "Jos Buttler",
+      "country": "England",
+      "image": "jos-butller.png",
+      "role": "Wicketkeeper-Batsman",
+      "battingType": "Right-handed",
+      "bowlingType": "Not Available",
+      "biddingPrice": 2900000
+    },
+    {
+      "id": 2,
+      "name": "Rashid Khan",
+      "country": "Afghanistan",
+      "image": "rashid-khan.png",
+      "role": "Bowler",
+      "battingType": "Right-handed",
+      "bowlingType": "Right-arm leg break",
+      "biddingPrice": 2500000
+    },
+    {
+      "id": 3,
+      "name": "Andre Russell",
+      "country": "West Indies",
+      "image": "andre-russell.png",
+      "role": "All-rounder",
+      "battingType": "Right-handed",
+      "bowlingType": "Right-arm fast",
+      "biddingPrice": 2300000
+    },
+    {
+      "id": 4,
+      "name": "Shaheen Afridi",
+      "country": "Pakistan",
+      "image": "shaheen-afridi.png",
+      "role": "Bowler",
+      "battingType": "Left-handed",
+      "bowlingType": "Left-arm fast",
+      "biddingPrice": 2000000
+    },
+    {
+      "id": 5,
+      "name": "Rohit Sharma",
+      "country": "India",
+      "image": "rohit-sharma.png",
+      "role": "Batsman",
+      "battingType": "Right-handed",
+      "bowlingType": "Right-arm off-break",
+      "biddingPrice": 1950000
+    },
+    {
+      "id": 6,
+      "name": "Joe Root",
+      "country": "England",
+      "image": "joe-root.png",
+      "role": "Batsman",
+      "battingType": "Right-handed",
+      "bowlingType": "Right-arm off-break",
+      "biddingPrice": 1800000
+    }
+  ]
+
+  function getPlayersData(){
+    return data
+  }
+  export {getPlayersData}
