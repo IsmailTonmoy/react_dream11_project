fbecb0473278863d5e49a1aa2163728304830699
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-10-31 22:12:57 +0600

    final complete

--- src/PlayerBox.jsx
+++ src/PlayerBox.jsx
@@ -9,7 +9,7 @@ export default function PlayerBox({player,onSelect}) {
 
 
     <div className="card bg-base-100 w-[340] shadow-xl">
-      <figure className="px-10 pt-10 w-96 h-96 ">
+      <figure className="pr-12 pl-5  pt-8 w-96 h-96 ">
         <img src={getImage(player.image)} alt="Pic" className="rounded-xl object-cover w-full h-full" />
       </figure>
       <div className="card-body">
